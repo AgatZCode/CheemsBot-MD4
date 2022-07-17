9ad0e0007070ac6ce37e02dce684c74be8b542df
AgatZ <98450255+AgatZCode@users.noreply.github.com>
2022-07-17 10:01:41 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -43,7 +43,7 @@ global.autoRecord = false //auto recording (true to on, false to off)
 global.available = true //auto available (true to on, false to off)
 
 //semt menu variants
-global.vidmenu = fs.readFileSync("./XeonMedia/theme/Cheems-bot.mp4") //gif and video menu
+global.vidmenu = fs.readFileSync("./XeonMedia/theme/Anko.mp4") //gif and video menu
 
 //documents variants
 global.doc1 = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
@@ -54,32 +54,32 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.vcardowner = ['0895336008585'] //ur owner number
+global.ownername = "ANKO BOT" //ur owner name
+global.ytname = "YT: Growds AD" //ur yt chanel name
+global.socialm = "GitHub: AgatZCode" //ur github or insta name
+global.location = "Indonesian, Japanese" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
-global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
-global.reactmoji = "😘" //ur menu react emoji
-global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
-global.author = "🦄Dream Guy Xeon" //ur sticker watermark author
+global.owner = ['0895336008585']
+global.ownertag = '0895336008585' //ur tag number
+global.botname = 'ANKO-BOT' //ur bot name
+global.linkz = "https://images.app.goo.gl/UbYfuv9Tj5SV4dbt6" //your theme url which will be displayed on whatsapp
+global.websitex = "https://images.app.goo.gl/UbYfuv9Tj5SV4dbt6" //ur website to be displayed
+global.botscript = 'https://github.com/DGXeon/AgatZCode' //script link
+global.reactmoji = "😎" //ur menu react emoji
+global.themeemoji = "😎" //ur theme emoji
+global.packname = "ANKOCHANN " //ur sticker watermark packname
+global.author = "ANKO CHANN 😱" //ur sticker watermark author
 
 //Bot theme media
-global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
-global.log0 = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur logo pic
-global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
-global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
+global.thum = fs.readFileSync("./XeonMedia/theme/anko.jpg") //ur thumb pic
+global.log0 = fs.readFileSync("./XeonMedia/theme/anko.jpg") //ur logo pic
+global.err4r = fs.readFileSync("./XeonMedia/theme/anko.jpg") //ur error pic
+global.thumb = fs.readFileSync("./XeonMedia/theme/anko.jpg") //ur thumb pic
 
 //damtabase
-global.premium = ['916909137213'] //ur premium numbers
+global.premium = ['0895336008585'] //ur premium numbers
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
